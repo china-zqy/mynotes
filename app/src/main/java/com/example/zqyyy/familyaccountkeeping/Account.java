4b21329f9398a93c38e777f76346f360bad003e8
QINAYANGZHu <1582185368@qq.com>
2020-12-16 17:00:31 +0800

    first commit

---
+++ app/src/main/java/com/example/zqyyy/familyaccountkeeping/Account.java
@@ -0,0 +1,24 @@
+package com.example.zqyyy.familyaccountkeeping;
+
+/**
+ * Created by zqy on 2020/12/13.
+ *
+
+这是一个测试
+ */
+public class Account {
+    private String name;
+    private int imageId;
+    public Account(String name, int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
+}
+
+
